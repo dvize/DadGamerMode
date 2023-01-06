62c1d80e59586e03d67db7edede11612c74139fa
dvize <daniel.m.han@gmail.com>
2023-01-05 18:41:05 -0800

    made it persistent across raids and added instant search and max stamina.

--- Plugin.cs
+++ Plugin.cs
@@ -3,78 +3,110 @@ using BepInEx.Configuration;
 using BepInEx.Logging;
 using Comfort.Common;
 using EFT;
-using System.Collections.Generic;
-using UnityEngine;
-using System.Timers;
 using System;
-using System.Threading;
-using System.Linq;
 
 namespace dvize.AILimit
 {
-    [BepInPlugin("com.dvize.GodModeTest", "dvize.GodModeTest", "1.0.0")]
+    [BepInPlugin("com.dvize.GodModeTest", "dvize.GodModeTest", "1.1.0")]
 
     public class Plugin : BaseUnityPlugin
     {
         public ConfigEntry<Boolean> Godmode { get; set; }
-        
+        public ConfigEntry<Boolean> CustomDamageMode { get; set; }
+        public ConfigEntry<int> CustomDamageModeVal { get; set; }
+        public ConfigEntry<Boolean> InstaSearch { get; private set; }
+        public ConfigEntry<Boolean> MaxStaminaToggle { get; set; }
         internal void Awake()
         {
             Godmode = Config.Bind("Player | Health", "Godmode", false, "Invincible");
+            MaxStaminaToggle = Config.Bind("Player | Skills", "Infinite Stamina", false, "Stamina Never Drains");
+            InstaSearch = Config.Bind("Player | Skills", "Instant Search", false);
+            CustomDamageMode = Config.Bind("Player | Health", "% Damage Received Enabled", false);
+            CustomDamageModeVal = Config.Bind("Player | Health", "% Damage Received Value", 100);
         }
 
-        bool runOnce = false;
         public static Player player;
         void Update()
         {
-            if (Godmode.Value == true)
+            if (GClass1748.InRaid)
             {
-                if (!runOnce)
+                player = Singleton<GameWorld>.Instance.MainPlayer;
+                if (Godmode.Value == true)
                 {
-                    player = Singleton<GameWorld>.Instance.MainPlayer;
-                    player.PlayerHealthController.SetDamageCoeff(-1f);
-                    player.PlayerHealthController.FallSafeHeight = 999999f;
-                    player.OnDamageReceived += FixDamage();
-                    runOnce = true;
+                    try
+                    {
+                        player.PlayerHealthController.SetDamageCoeff(-1f);
+                        player.PlayerHealthController.FallSafeHeight = 999999f;
+                        //player.PlayerHealthController.ChangeHealth(EBodyPart.Head, 9999999, new DamageInfo());
+
+                        player.PlayerHealthController.RemoveNegativeEffects(EBodyPart.Common);
+                        player.PlayerHealthController.RestoreFullHealth();
+
+                    }
+                    catch
+                    {
+                        Logger.LogInfo("Error GodMode On.");
+                    }
+
                 }
-                
-            }
-            else
-            {
-                if (runOnce)
+                else {
+                    try
+                    {
+                        player.PlayerHealthController.SetDamageCoeff(1f);
+                        player.PlayerHealthController.FallSafeHeight = 1.5f;
+                    }
+                    catch
+                    {
+                        Logger.LogInfo("Error GodMode Off.");
+                    }
+                }
+
+                if (CustomDamageMode.Value == true && Godmode.Value != true)
                 {
-                    player.PlayerHealthController.SetDamageCoeff(1f);
-                    player.PlayerHealthController.FallSafeHeight = 1.5f;
-                    player.OnDamageReceived -= FixDamage();
-                    runOnce = false;
+                    try
+                    {
+                        player.PlayerHealthController.SetDamageCoeff((float)(CustomDamageModeVal.Value/100));
+                    }
+                    catch
+                    {
+                        Logger.LogInfo("Error GodMode Off.");
+                    }
                 }
-            }
-        }
-        private Player.GDelegate42 FixDamage()
-        {
-            player.PlayerHealthController.RemoveNegativeEffects(EBodyPart.Common);
-            player.PlayerHealthController.RemoveNegativeEffects(EBodyPart.Head);
-            player.PlayerHealthController.RemoveNegativeEffects(EBodyPart.Stomach);
-            player.PlayerHealthController.RemoveNegativeEffects(EBodyPart.Chest);
-            player.PlayerHealthController.RemoveNegativeEffects(EBodyPart.LeftArm);
-            player.PlayerHealthController.RemoveNegativeEffects(EBodyPart.LeftLeg);
-            player.PlayerHealthController.RemoveNegativeEffects(EBodyPart.RightArm);
-            player.PlayerHealthController.RemoveNegativeEffects(EBodyPart.RightLeg);
-            
-            player.Heal(EBodyPart.Head, 999999f);
-            player.Heal(EBodyPart.Stomach, 999999f);
-            player.Heal(EBodyPart.Chest, 999999f);
-            player.Heal(EBodyPart.LeftArm, 999999f);
-            player.Heal(EBodyPart.LeftLeg, 999999f);
-            player.Heal(EBodyPart.RightArm, 999999f);
-            player.Heal(EBodyPart.RightLeg, 999999f);
-            
-            player.PlayerHealthController.RestoreFullHealth();
-            
-            return null;
-        }
+
+                if (MaxStaminaToggle.Value == true)
+                {
+                    try
+                    {
+                        player.Physical.Stamina.Current = player.Physical.Stamina.TotalCapacity.Value;
+                        player.Physical.HandsStamina.Current = player.Physical.HandsStamina.TotalCapacity.Value;
+                        player.Physical.Oxygen.Current = player.Physical.Oxygen.TotalCapacity.Value;
+                    }
+                    catch
+                    {
+                        Logger.LogInfo("Error MaxStamina");
+                    }
+                }
+
+                if(InstaSearch.Value == true)
+                {
+                    try
+                    {
+                        player.Skills.AttentionEliteExtraLootExp.Value = true;
+                        player.Skills.AttentionEliteLuckySearch.Value = 100f;
+                        player.Skills.IntellectEliteContainerScope.Value = true;
+                    }
+                    catch
+                    {
+                        Logger.LogInfo("Error Instasearch");
+                    }
+                    
+                }
 
 
+            }
+            
+        }
+        
 
     }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("dvize.AILimit")]
+[assembly: AssemblyTitle("dvize.GodModeTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("dvize.AILimit")]
+[assembly: AssemblyProduct("dvize.GodModeTest")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
